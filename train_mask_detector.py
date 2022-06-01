4be17ea456b303b0e8efce0f3908805333710714
kokokorev <train.spot.spotting@gmail.com>
2022-06-01 06:28:38 +0400

    определение qr кода с вебки ПК

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -20,22 +20,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# initialize the initial learning rate, number of epochs to train for,
-# and batch size
-INIT_LR = 1e-4
+# 20 эпох
 EPOCHS = 20
+# коэффициент скорости обучения - отвечает за величину коррекции весов
+INIT_LR = 1e-4
+# разрем одного батча
+# батч - часть датасета, что бы прогонять в одной эпохе веь датасет по частям
 BS = 32
 
-DIRECTORY = r"D:\dev\python\Face-Mask-Detection\dataset"
+DIRECTORY = r"D:\dev\python\face-mask-detection\dataset"
 CATEGORIES = ["with_mask", "without_mask"]
 
-# grab the list of images in our dataset directory, then initialize
-# the list of data (i.e., images) and class images
+# собираю картинки из датасета
 print("[INFO] loading images...")
-
 data = []
 labels = []
-
 for category in CATEGORIES:
     path = os.path.join(DIRECTORY, category)
     for img in os.listdir(path):
@@ -47,7 +46,7 @@ for category in CATEGORIES:
         data.append(image)
         labels.append(category)
 
-# perform one-hot encoding on the labels
+# готовим нейронку
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
@@ -58,7 +57,7 @@ labels = np.array(labels)
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
     test_size=0.20, stratify=labels, random_state=42)
 
-# construct the training image generator for data augmentation
+# из существующего датасета генерирую новые картинки изменяя старые
 aug = ImageDataGenerator(
     rotation_range=20,
     zoom_range=0.15,
@@ -68,13 +67,11 @@ aug = ImageDataGenerator(
     horizontal_flip=True,
     fill_mode="nearest")
 
-# load the MobileNetV2 network, ensuring the head FC layer sets are
-# left off
+# настраиваю модель перед обучением
+# настраиваем input нейронки
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
     input_tensor=Input(shape=(224, 224, 3)))
 
-# construct the head of the model that will be placed on top of the
-# the base model
 headModel = baseModel.output
 headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
 headModel = Flatten(name="flatten")(headModel)
@@ -82,22 +79,17 @@ headModel = Dense(128, activation="relu")(headModel)
 headModel = Dropout(0.5)(headModel)
 headModel = Dense(2, activation="softmax")(headModel)
 
-# place the head FC model on top of the base model (this will become
-# the actual model we will train)
 model = Model(inputs=baseModel.input, outputs=headModel)
 
-# loop over all layers in the base model and freeze them so they will
-# *not* be updated during the first training process
 for layer in baseModel.layers:
     layer.trainable = False
 
-# compile our model
 print("[INFO] compiling model...")
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
     metrics=["accuracy"])
 
-# train the head of the network
+# обучение
 print("[INFO] training head...")
 H = model.fit(
     aug.flow(trainX, trainY, batch_size=BS),
@@ -106,23 +98,18 @@ H = model.fit(
     validation_steps=len(testX) // BS,
     epochs=EPOCHS)
 
-# make predictions on the testing set
 print("[INFO] evaluating network...")
 predIdxs = model.predict(testX, batch_size=BS)
 
-# for each image in the testing set we need to find the index of the
-# label with corresponding largest predicted probability
 predIdxs = np.argmax(predIdxs, axis=1)
 
-# show a nicely formatted classification report
 print(classification_report(testY.argmax(axis=1), predIdxs,
     target_names=lb.classes_))
 
-# serialize the model to disk
 print("[INFO] saving mask detector model...")
 model.save("mask_detector.model", save_format="h5")
 
-# plot the training loss and accuracy
+# вывожу график обучения
 N = EPOCHS
 plt.style.use("ggplot")
 plt.figure()
